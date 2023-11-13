404e1943349af0e378542005e23b90499f89c322
Leonardo <leoruizz14@outlook.com>
2023-11-13 17:09:10 -0300

    add content to highlights screen

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/highlight_item.dart';
 
 class Highlights extends StatelessWidget {
   const Highlights({super.key});
   final List items = destaques;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: CustomScrollView(),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(
+              child: Padding(
+            padding: EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              'Destaques',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontSize: 32,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          )),
+          SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+            return HighlightItem(
+              imageURI: items[index]['image'],
+              itemTitle: items[index]['name'],
+              itemPrice: items[index]['price'],
+              itemDescription: items[index]['description'],
+            );
+          }, childCount: items.length))
+        ],
+      ),
     );
   }
 }
