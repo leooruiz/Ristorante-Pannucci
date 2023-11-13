7eafc09859bc53892c593f317e9058383cf943a2
Leonardo <leoruizz14@outlook.com>
2023-11-13 17:08:38 -0300

    create DrinkScreen

---
+++ lib/screens/drinks_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrinkScreen extends StatelessWidget {
+  const DrinkScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
