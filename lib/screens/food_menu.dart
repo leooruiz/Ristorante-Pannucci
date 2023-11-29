4f777f8efc76deeecf81192ba2738996baedc0a8
Leonardo Ruiz <leoruizz14@outlook.com>
2023-11-28 21:18:07 -0300

    Sliver list to food menu

--- lib/screens/food_menu.dart
+++ lib/screens/food_menu.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/components/food_item.dart';
+import 'package:panucci_ristorante/cardapio.dart';
 
 class FoodMenu extends StatelessWidget {
-  const FoodMenu({super.key});
-
+  FoodMenu({super.key});
+  final List items = comidas;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  fontFamily: 'Caveat',
-                  fontSize: 32,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    fontFamily: 'Caveat',
+                    fontSize: 32,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
-          ),
-          SliverList(delegate: SliverChildBuilderDelegate((context, index) {}))
-        ],
+            SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+              return FoodItem(
+                itemTitle: items[index]['name'],
+                itemPrice: items[index]['price'],
+                imageURI: items[index]['image'],
+              );
+            }, childCount: items.length),)
+          ],
+        ),
       ),
     );
   }
