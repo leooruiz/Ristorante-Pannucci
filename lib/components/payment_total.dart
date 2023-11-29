dd2a109489dc9793c5b3730024d6e6559242ee91
Leonardo Ruiz <leoruizz14@outlook.com>
2023-11-28 21:13:31 -0300

    adding padding to payment total

--- lib/components/payment_total.dart
+++ lib/components/payment_total.dart
@@ -6,59 +6,62 @@ class PaymentTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text("Pedido:",
-                  style: TextStyle(color: AppColors.paymentMethodReceiptColor)),
-              const Text("R\$ 154,80"),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Pedido:",
+                    style: TextStyle(color: AppColors.paymentMethodReceiptColor)),
+                const Text("R\$ 154,80"),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text("Serviço (10%):",
-                  style: TextStyle(color: AppColors.paymentMethodReceiptColor)),
-              const Text("R\$ 15,48"),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Serviço (10%):",
+                    style: TextStyle(color: AppColors.paymentMethodReceiptColor)),
+                const Text("R\$ 15,48"),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text("Total:",
-                  style: TextStyle(color: AppColors.paymentMethodReceiptColor)),
-              const Text("R\$ 170,28",
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16)),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Total:",
+                    style: TextStyle(color: AppColors.paymentMethodReceiptColor)),
+                const Text("R\$ 170,28",
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16)),
+              ],
+            ),
           ),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          style: AppColors.buttonStyle,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(right: 8.0),
-                  child: Icon(Icons.account_balance_wallet),
-                ),
-                Text(
-                  "Pedir",
-                  style: TextStyle(fontWeight: FontWeight.w500),
-                )
-              ]),
-        )
-      ],
+          ElevatedButton(
+            onPressed: () {},
+            style: AppColors.buttonStyle,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 8.0),
+                    child: Icon(Icons.account_balance_wallet),
+                  ),
+                  Text(
+                    "Pedir",
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  )
+                ]),
+          )
+        ],
+      ),
     );
   }
 }
